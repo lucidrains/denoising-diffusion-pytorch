27853452a2a7a17ff17643f85e95ba9ffdc7b544
Phil Wang <lucidrains@gmail.com>
2022-05-13 12:52:11 -0700

    add gaussian diffusion where model predicts both noise and x_start, with a learned weighting between the two (experimental)

--- denoising_diffusion_pytorch/__init__.py
+++ denoising_diffusion_pytorch/__init__.py
@@ -1,2 +1,4 @@
 from denoising_diffusion_pytorch.denoising_diffusion_pytorch import GaussianDiffusion, Unet, Trainer
+
 from denoising_diffusion_pytorch.learned_gaussian_diffusion import LearnedGaussianDiffusion
+from denoising_diffusion_pytorch.weighted_objective_gaussian_diffusion import WeightedObjectiveGaussianDiffusion
