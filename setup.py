c78709f88751ae5f998f16ac7442473b83f5111c
Phil Wang <lucidrains@gmail.com>
2022-08-31 06:20:59 -0700

    0.27.6

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'denoising-diffusion-pytorch',
   packages = find_packages(),
-  version = '0.27.5',
+  version = '0.27.6',
   license='MIT',
   description = 'Denoising Diffusion Probabilistic Models - Pytorch',
   author = 'Phil Wang',
