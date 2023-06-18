c6f373c8587dec47a3d27ec5b4c3f027011b6c81
Adversarian <a77physics@gmail.com>
2023-06-18 10:01:52 +0430

    FID eval module added.

---
+++ denoising_diffusion_pytorch/fid_evaluation.py
@@ -0,0 +1,90 @@
+import torch
+import numpy as np
+import os
+import math
+from tqdm.auto import tqdm
+from einops import rearrange, repeat
+from pytorch_fid.fid_score import calculate_frechet_distance
+from pytorch_fid.inception import InceptionV3
+from denoising_diffusion_pytorch import num_to_groups
+
+class FIDEvaluation():
+    def __init__(
+            self, 
+            batch_size,
+            dl, 
+            sampler,
+            channels=3, 
+            accelerator=None, 
+            stats_dir="./results", 
+            device="cuda", 
+            num_fid_samples=50000, 
+            inception_block_idx=2048
+        ):
+        self.batch_size = batch_size
+        self.n_samples = num_fid_samples
+        self.device = device
+        self.channels = channels
+        self.dl = dl
+        self.sampler = sampler
+        self.stats_dir = stats_dir
+        self.print_fn = print if accelerator is None else accelerator.print
+        assert inception_block_idx in InceptionV3.BLOCK_INDEX_BY_DIM
+        block_idx = InceptionV3.BLOCK_INDEX_BY_DIM[inception_block_idx]
+        self.inception_v3 = InceptionV3([block_idx]).to(device)
+        self.dataset_stats_loaded = False
+
+    def calculate_inception_features(self, samples):
+        if self.channels == 1:
+            samples = repeat(samples, "b 1 ... -> b c ...", c=3)
+        features = self.inception_v3(samples)[0]
+        features = rearrange(features, "... 1 1 -> ...")
+        return features
+
+    def load_or_precalc_dataset_stats(self):
+        path = os.path.join(self.stats_dir, "dataset_stats.pkl")
+        try:
+            ckpt = np.load(path)
+            self.m2, self.s2 = ckpt["m2"], ckpt["s2"]
+            self.print_fn("Dataset stats loaded from disk.")
+        except FileNotFoundError:
+            num_batches = int(math.ceil(self.n_samples/self.batch_size))
+            stacked_real_features = []
+            self.print_fn(
+                f"Stacking Inception features for {self.n_samples} samples from the real dataset."
+            )
+            for i in tqdm(range(num_batches)):
+                try:
+                    real_samples = next(self.dl)
+                except StopIteration:
+                    break
+                real_samples = real_samples.to(self.device)
+                real_features = self.calculate_inception_features(real_samples)
+                stacked_real_features.append(real_features)
+            stacked_real_features = torch.cat(stacked_real_features, dim=0).cpu().numpy()
+            m2 = np.mean(stacked_real_features, axis=0)
+            s2 = np.cov(stacked_real_features, rowvar=False)
+            np.savez_compressed(path, m2=m2, s2=s2)
+            self.print_fn(f"Dataset stats cached to {path} for future use.")
+            self.m2, self.s2 = m2, s2
+        self.dataset_stats_loaded = True
+    
+    @torch.inference_mode()
+    def fid_score(self):
+        if not self.dataset_stats_loaded:
+            self.load_or_precalc_dataset_stats()
+
+        batches = num_to_groups(self.n_samples, self.batch_size)
+        stacked_fake_features = []
+        self.print_fn(
+            f"Stacking Inception features for {self.n_samples} generated samples."
+        )
+        for batch in tqdm(batches):
+            fake_samples = self.sampler.sample(batch_size=batch)
+            fake_features = self.calculate_inception_features(fake_samples)
+            stacked_fake_features.append(fake_features)
+        stacked_fake_features = torch.cat(stacked_fake_features, dim=0).cpu().numpy()
+        m1 = np.mean(stacked_fake_features, axis=0)
+        s1 = np.cov(stacked_fake_features, rowvar=False)
+
+        return calculate_frechet_distance(m1, s1, self.m2, self.s2)
