3f2ef250dcc57e2f1ec29a7c6a98430c0d8da5ef
lucidrains <lucidrains@gmail.com>
2023-09-10 19:17:09 -0700

    address https://github.com/lucidrains/denoising-diffusion-pytorch/issues/249

--- denoising_diffusion_pytorch/denoising_diffusion_pytorch.py
+++ denoising_diffusion_pytorch/denoising_diffusion_pytorch.py
@@ -112,11 +112,12 @@ class SinusoidalPosEmb(nn.Module):
     def __init__(self, dim, theta = 10000):
         super().__init__()
         self.dim = dim
+        self.theta = theta
 
     def forward(self, x):
         device = x.device
         half_dim = self.dim // 2
-        emb = math.log(theta) / (half_dim - 1)
+        emb = math.log(self.theta) / (half_dim - 1)
         emb = torch.exp(torch.arange(half_dim, device=device) * -emb)
         emb = x[:, None] * emb[None, :]
         emb = torch.cat((emb.sin(), emb.cos()), dim=-1)
