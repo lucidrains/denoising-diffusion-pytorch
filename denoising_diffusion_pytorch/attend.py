b83c401ade6917eb8cafe0984dcf451d0a5b9de9
lucidrains <lucidrains@gmail.com>
2024-06-21 07:13:55 -0700

    address https://github.com/lucidrains/denoising-diffusion-pytorch/issues/328

--- denoising_diffusion_pytorch/attend.py
+++ denoising_diffusion_pytorch/attend.py
@@ -60,7 +60,7 @@ class Attend(nn.Module):
 
         device_properties = torch.cuda.get_device_properties(torch.device('cuda'))
 
-        if device_properties.major == 8 and device_properties.minor == 0:
+        if device_properties.major >= 8 and device_properties.minor == 0:
             print_once('A100 GPU detected, using flash attention if input tensor is on cuda')
             self.cuda_config = AttentionConfig(True, False, False)
         else:
